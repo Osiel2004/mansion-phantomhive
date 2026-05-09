8ebfffe55fbf82be7e0b7cedbaf7a7de15a689bc
Osiel2004 <al22020345@itsa.edu.mx>
2026-05-08 23:36:05 -0600

    Agregado el envio de correo

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_cognito_user_pool" "pool_usuarios" {
   auto_verified_attributes = ["email"]
 
   password_policy {
-    minimum_length    = 8
+    minimum_length    = 8 
     require_lowercase = true
     require_numbers   = true
     require_uppercase = true
